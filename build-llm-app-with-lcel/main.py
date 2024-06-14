0c27afa6ff73f718fd9582c8971e5cde0fb53b68
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-06-13 21:01:33 -0500

    learning: chaining

--- build-llm-app-with-lcel/main.py
+++ build-llm-app-with-lcel/main.py
@@ -1,8 +1,21 @@
 import os
 from dotenv import load_dotenv
 from langchain_anthropic import ChatAnthropic
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
 model = ChatAnthropic(model="claude-3-haiku-20240307")
+parser = StrOutputParser()
 
+messages = [
+  SystemMessage(content="Translate the following from English into Italian"),
+  HumanMessage(content="hi!"),
+]
+
+chain = model | parser
+
+res = chain.invoke(messages)
+
+print(res)
