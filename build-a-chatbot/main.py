ea86c23d865f2103988f1d618081140f6960bc7f
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2024-06-14 14:27:55 -0500

    learning: chatting in different languages and streaming responses

--- build-a-chatbot/main.py
+++ build-a-chatbot/main.py
@@ -4,6 +4,9 @@ from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_core.messages import HumanMessage
 from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import RunnablePassthrough
+
 
 load_dotenv()
 
@@ -14,14 +17,35 @@ def get_session_history(session_id: str) -> BaseChatMessageHistory:
     store[session_id] = ChatMessageHistory()
   return store[session_id]
 
+def filter_messages(messages, k=10):
+  return messages[-k:]
+
 model = ChatAnthropic(model="claude-3-haiku-20240307")
 
-with_message_history = RunnableWithMessageHistory(model, get_session_history)
+prompt = ChatPromptTemplate.from_messages([
+  ("system", "You are a helpful assistant. Answer all questions to the best of your ability in {language}."), 
+  MessagesPlaceholder(variable_name="messages")
+])
+
+chain = (
+  RunnablePassthrough.assign(messages=lambda x: filter_messages(x["messages"]))
+  | prompt
+  | model
+)
+
+chain = prompt | model
+
+with_message_history = RunnableWithMessageHistory(chain, get_session_history, input_messages_key="messages")
 
 config = {"configurable": {"session_id": "1"}}
 
 while True:
   user_input = input("You: ")
   message = HumanMessage(content=user_input)
-  response = with_message_history.invoke(message, config)
-  print("Bot:", response.content)
+  print("Bot: ", end="")
+  for r in with_message_history.stream(
+    { "messages": HumanMessage(content=user_input), "language": "English"},
+    config=config
+  ):
+    print(r.content, end="")
+  print()
